0345903d353851f289d90c3d83e47e093e66b5ed
GyuHee Jo <gyuheejo@igg.com.co>
2025-02-11 14:12:25 -0500

    router changed

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from 'next/server';
 
 export const GET = () => {
     const allCookies = cookies().getAll();
-    // const cookieDomain = process.env.HOST ?? 'localhost';
-    const cookieDomain = 'localhost';
+    const cookieDomain = process.env.HOST ?? 'localhost';
+    //const cookieDomain = 'localhost';
     
 
     allCookies.forEach((cookie) => {
